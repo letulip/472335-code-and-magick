9e3f9df05bb0154ad348503a349568dc6f0d21f3
Igor Vladimirskiy <igor.vladimirskiy@wwpass.com>
2017-11-15 13:40:49 +0300

    Добавлены переменные функции из задания

--- js/update.js
+++ js/update.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+function getFireballSpeed(left) {
+  if (left) {
+    return 5;
+  }
+  return 2;
+}
+
+function getWizardHeight() {
+  return (1.337 * wizardWidth);
+}
+
+function getWizardX(width) {
+  return ((width - wizardWidth) / 2);
+}
+
+function getWizardY(height) {
+  return (height * 0,67);
+}
