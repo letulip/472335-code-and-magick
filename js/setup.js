b88565d60243307680acfe1e35ff5f3c4c9720e4
Igor Vladimirskiy <igor.vladimirskiy@wwpass.com>
2017-11-22 13:21:00 +0300

    Скорректированы переменные и логика createMages

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,10 @@
 'use strict';
 
 var userDialog = document.querySelector('.setup');
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
-var fragment = document.createDocumentFragment();
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var lastnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-var mages = [];
 
 function Mage(name, coatColor, eyesColor) {
   this.name = name;
@@ -29,12 +25,15 @@ function getRandomColor(colors) {
 }
 
 function createMages() {
+  var mages = [];
   for (var i = 0; i < 4; i++) {
     mages.push(new Mage(getRandomName(), getRandomColor(coatColors), getRandomColor(eyesColors)));
   }
+  return mages;
 }
 
 function createMageElement(mage) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = mage.name;
   wizardElement.querySelector('.wizard-coat').style.fill = mage.coatColor;
@@ -42,7 +41,9 @@ function createMageElement(mage) {
   return wizardElement;
 }
 
-function fillFragment() {
+function fillFragment(mages) {
+  var fragment = document.createDocumentFragment();
+  var similarListElement = document.querySelector('.setup-similar-list');
   for (var i = 0; i < mages.length; i++) {
     fragment.appendChild(createMageElement(mages[i]));
   }
@@ -50,6 +51,5 @@ function fillFragment() {
 }
 
 userDialog.classList.remove('hidden');
-createMages();
-fillFragment();
+fillFragment(createMages());
 document.querySelector('.setup-similar').classList.remove('hidden');
