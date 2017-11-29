85908814d788e44026cd592a3d50ef79af9a0c3a
Igor Vladimirskiy <igor.vladimirskiy@wwpass.com>
2017-11-29 15:19:04 +0300

    Добавлена демо-валидация имени персонажа

--- js/setup.js
+++ js/setup.js
@@ -12,6 +12,26 @@
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
+  var userNameInput = setup.addEventListener('invalid', function (evt) {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
 
   function showBlock(element) {
     element.classList.remove('hidden');
