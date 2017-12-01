8526566f63d00d716744f75455f8b4715b68e02f
Igor Vladimirskiy <igor.vladimirskiy@wwpass.com>
2017-12-01 11:46:07 +0300

    Скорректирована группировка переменных и функций, добавлена функция changeColors

--- js/setup.js
+++ js/setup.js
@@ -1,55 +1,22 @@
 'use strict';
 
 (function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
   var setup = document.querySelector('.setup');
-  // var setupSimilar = document.querySelector('.setup-similar');
   var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
   var lastnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
   var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
+  var setupWizardForm = setup.querySelector('.setup-wizard-form');
   var userNameInput = setup.querySelector('.setup-user-name');
-  userNameInput.addEventListener('invalid', function () {
-    if (userNameInput.validity.tooShort) {
-      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-    } else if (userNameInput.validity.tooLong) {
-      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
-    } else if (userNameInput.validity.valueMissing) {
-      userNameInput.setCustomValidity('Обязательное поле');
-    } else {
-      userNameInput.setCustomValidity('');
-    }
-  });
-  userNameInput.addEventListener('input', function (evt) {
-    var target = evt.target;
-    if (target.value.length < 2) {
-      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-    } else {
-      target.setCustomValidity('');
-    }
-  });
   var setupWizard = document.querySelector('.setup-wizard-wrap');
   var coatColorChange = setupWizard.querySelector('.wizard-coat');
-  coatColorChange.addEventListener('click', function () {
-    coatColorChange.style.fill = getRandomColor(coatColors);
-  });
   var eyesColorChange = setupWizard.querySelector('.wizard-eyes');
-  eyesColorChange.addEventListener('click', function () {
-    eyesColorChange.style.fill = getRandomColor(eyesColors);
-  });
-
   var setupFireball = document.querySelector('.setup-fireball-wrap');
-  setupFireball.addEventListener('click', function () {
-    setupFireball.style.backgroundColor = getRandomColor(fireballColors);
-  });
-
-  // function showBlock(element) {
-  //   element.classList.remove('hidden');
-  // }
 
   function onPopupEscPress(evt) {
     if (evt.keyCode === ESC_KEYCODE) {
@@ -59,12 +26,15 @@
 
   function openPopup() {
     setup.classList.remove('hidden');
-    setupClose.addEventListener('keydown', onPopupEscPress);
+    setup.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        closePopup();
+      }
+    });
   }
 
   function closePopup() {
     setup.classList.add('hidden');
-    setupClose.removeEventListener('keydown', onPopupEscPress);
   }
 
   function Mage(name, coatColor, eyesColor) {
@@ -83,6 +53,10 @@
 
   function getRandomColor(colors) {
     return colors[getRandomInt(0, colors.length)];
+  }
+
+  function changeColor(target, colorsArray) {
+    target.style.fill = getRandomColor(colorsArray);
   }
 
   function createMages() {
@@ -131,7 +105,38 @@
     }
   });
 
-  // showBlock(setup);
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  userNameInput.addEventListener('input', function (evt) {
+    var target = evt.target;
+    if (target.value.length < 2) {
+      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+
+  coatColorChange.addEventListener('click', function () {
+    changeColor(coatColorChange, coatColors);
+  });
+
+  eyesColorChange.addEventListener('click', function () {
+    changeColor(eyesColorChange, eyesColors);
+  });
+
+  setupFireball.addEventListener('click', function () {
+    setupFireball.style.backgroundColor = getRandomColor(fireballColors);
+  });
+
   fillFragment(createMages());
-  // showBlock(setupSimilar);
 }());
