06b526a98b2a187fee8a9ef23955c19b5f66ba31
Igor Vladimirskiy <igor.vladimirskiy@wwpass.com>
2017-12-05 18:48:50 +0300

    CreateMageElement перенесена в mage.js

--- js/fillTemplateFragment.js
+++ js/fillTemplateFragment.js
@@ -6,7 +6,7 @@ window.fillTemplateFragment = (function () {
       var fragment = document.createDocumentFragment();
       var similarListElement = document.querySelector('.setup-similar-list');
       for (var i = 0; i < mages.length; i++) {
-        fragment.appendChild(window.createMage.createMageElement(mages[i]));
+        fragment.appendChild(window.mage.createMageElement(mages[i]));
       }
       similarListElement.appendChild(fragment);
     }
--- js/mage.js
+++ js/mage.js
@@ -40,6 +40,14 @@ window.mage = (function () {
     },
     changeColor: function (target, colorsArray) {
       target.style.fill = window.mage.getRandomColor(colorsArray);
+    },
+    createMageElement: function (mage) {
+      var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+      var wizardElement = similarWizardTemplate.cloneNode(true);
+      wizardElement.querySelector('.setup-similar-label').textContent = mage.name;
+      wizardElement.querySelector('.wizard-coat').style.fill = mage.coatColor;
+      wizardElement.querySelector('.wizard-eyes').style.fill = mage.eyesColor;
+      return wizardElement;
     }
   };
 })();
