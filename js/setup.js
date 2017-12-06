429d1dfa28bc747f323a64fc66e26c01f3421ca1
Igor Vladimirskiy <igor.vladimirskiy@wwpass.com>
2017-12-06 14:40:36 +0300

    Добавлен dialog.js и перетаскивание инвентаря

--- js/setup.js
+++ js/setup.js
@@ -2,11 +2,43 @@
 
 (function () {
   var setupOpen = document.querySelector('.setup-open');
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+  var artifactsElement = document.querySelector('.setup-artifacts');
 
   setupOpen.addEventListener('click', window.popup.openPopup);
 
   setupOpen.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, window.popup.openPopup);
+  });
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+    }
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
   });
 
   window.fillFragment(window.mage.createMages());
